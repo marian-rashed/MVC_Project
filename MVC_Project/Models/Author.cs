be019e08088375a031754753dcd58afd4ba8739b
marian <marian.rashed23@gmail.com>
2024-04-05 09:55:32 +0200

    from marian to maria(validation in author model)

--- MVC_Project/Models/Author.cs
+++ MVC_Project/Models/Author.cs
@@ -12,12 +12,16 @@ namespace MVC_Project
         [Key]
         public int AuthorId { get; set; }
 
+        [MaxLength(25, ErrorMessage = "Name must be between 10 to 25 letter"), MinLength(10),]
+        [RegularExpression(@"^[A-Za-z ]+$", ErrorMessage = "Name must be Letters only")]
         public string AuthorName { get; set; }
 
+        [MaxLength(100), MinLength(50)]
         public string Biography { get; set; }
 
         public DateTime BirthDate { get; set; }
 
+        [RegularExpression(@"^[A-Za-z ]+$", ErrorMessage = "Country must be Letters only")]
         public string Country { get; set; }
 
         public string ImageUrl { get; set; }
