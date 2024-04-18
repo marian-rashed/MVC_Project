a51144324b631f97fb1ff5abc183466ffb2dade7
Youstina Emad <tenaemad934@gmail.com>
2024-04-18 17:09:19 +0200

    ok

---
+++ MVC_Project/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Project.Controllers
+{
+	public class DashboardController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ MVC_Project/Hubs/ReviewsHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.CodeAnalysis;
+using MVC_Project.Models;
+
+namespace MVC_Project.Hubs
+{
+	public class ReviewsHub:Hub
+	{
+        private readonly BookStoreContext bookStore;
+        private readonly UserManager<ApplicationUser> userManager;
+        public ReviewsHub(BookStoreContext bookStore, UserManager<ApplicationUser> userManager)
+		{
+            this.bookStore = bookStore;
+            this.userManager = userManager;
+        }
+		public async Task SendReview(string message, int bookId)
+		{
+			var CurrentCustomer = await userManager.GetUserAsync(Context.User);
+			string name = CurrentCustomer.UserName;
+
+			var review = new Review
+			{				
+				User=CurrentCustomer,				
+				Text = message,
+				BookID = bookId
+			};
+			bookStore.Reviews.Add(review);
+			await bookStore.SaveChangesAsync();
+			await Clients.All.SendAsync("RecieveNewComment", name, message, bookId);
+			
+		}
+	}
+}
